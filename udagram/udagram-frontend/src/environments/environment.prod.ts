bcfc3929841814f0e4b2836053947ac6dd274f73
AbdoAyman2001 <abdo.ah884@gmail.com>
2022-08-09 13:13:34 +0200

    last commit inshallah

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-env.eba-k53drkm2.us-east-1.elasticbeanstalk.com/api/v0/',
+  apiHost: 'http://udagram-api-env.eba-k53drkm2.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
